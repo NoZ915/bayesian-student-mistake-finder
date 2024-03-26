6295b71fe4e460eb7c0a13367e407d07298bdd01
NoZ915 <tina0915tw0915@gmail.com>
2024-03-26 15:42:17 +0800

    added:backend server port

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-// nothing
+const express = require("express");
+require("dotenv").config();
+const app = express();
+
+app.get("/", (req, res) => {
+    res.json({mssg: "Welcome to tha app"})
+})
+
+app.listen(process.env.PORT, () => {
+    console.log("listen to port 4000!", process.env.PORT)
+})
